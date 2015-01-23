3df855287d62d255b831df8b023add1da4a38ee6
charlierproctor <charlie.proctor@yahoo.com>
2015-01-22 21:35:02 -0500

    linking up the buttons / labels

--- ToEatOrNotToEat/MainVC.swift
+++ ToEatOrNotToEat/MainVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkButtonAction(sender: AnyObject) {
+    }
 
 }
 
